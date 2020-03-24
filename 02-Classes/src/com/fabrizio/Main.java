9db467c73e8b6f3c6cdf2ad25c7c60d92a75c0f4
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-24 16:44:22 -0400

    memory allocation

--- 02-Classes/src/com/fabrizio/Main.java
+++ 02-Classes/src/com/fabrizio/Main.java
@@ -4,11 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	    var textBox1 = new TextBox();
-	    textBox1.setText("Box 1");
-        System.out.println(textBox1.text.toUpperCase());
-
-        var textBox2 = new TextBox();
-        textBox2.setText("Box 2");
-        System.out.println(textBox2.text);
+	    var textBox2 = textBox1;
+	    textBox2.setText(("Hello World"));
+        System.out.println(textBox1.text);
     }
 }
