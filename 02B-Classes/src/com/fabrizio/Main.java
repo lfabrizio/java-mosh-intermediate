07081ff9f48ff3d002a8415a846d2e020cc39171
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-02 19:52:37 -0400

    3

--- 02B-Classes/src/com/fabrizio/Main.java
+++ 02B-Classes/src/com/fabrizio/Main.java
@@ -3,11 +3,6 @@ package com.fabrizio;
 public class Main {
 
     public static void main(String[] args) {
-        var employee = new Employee();
-        employee.setBaseSalary(-1);
-        employee.baseSalary(50_000);
-        employee.setHourlyRate(20);
-        int wage = employee.calculateWage(10);
-        System.out.println(wage);
+        Browser browser = new Browser();
     }
 }
