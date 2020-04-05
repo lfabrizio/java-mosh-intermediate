8f0c1c5bb7e36e937d90dc8ca0f61030a9a2e09f
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-05 15:32:45 -0400

    overloading methods

--- 03-Refactoring/src/com/fabrizio/Console.java
+++ 03-Refactoring/src/com/fabrizio/Console.java
@@ -6,11 +6,9 @@ public class Console {
     private static Scanner scanner = new Scanner(System.in);
 
     public static double readNumber(String prompt) {
-
         return scanner.nextDouble();
     }
     public static double readNumber(String prompt, double min, double max) {
-
         double value;
         while (true) {
             System.out.print(prompt);
