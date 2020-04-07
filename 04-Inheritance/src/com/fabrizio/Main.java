b083ad735ad4b6c3b571ff0e1dfef7e3e004b6f0
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-07 18:55:36 -0400

    constructors

--- 04-Inheritance/src/com/fabrizio/Main.java
+++ 04-Inheritance/src/com/fabrizio/Main.java
@@ -3,9 +3,7 @@ package com.fabrizio;
 public class Main {
     // referencing  same object on memory
     public static void main(String[] args) {
-	var box1 = new TextBox();
-	var box2 = new TextBox();
-        System.out.println(box1.toString());
+	var textBox = new TextBox();
 
 
     }
--- 04-Inheritance/src/com/fabrizio/UIControl.java
+++ 04-Inheritance/src/com/fabrizio/UIControl.java
@@ -3,6 +3,11 @@ package com.fabrizio;
 public class UIControl {
     private boolean isEnabled = true;
 
+    public UIControl(boolean isEnabled) {
+        this.isEnabled= isEnabled;
+        System.out.println("Ui Control");
+    }
+
     public void enable(){
         isEnabled = true;
     }
