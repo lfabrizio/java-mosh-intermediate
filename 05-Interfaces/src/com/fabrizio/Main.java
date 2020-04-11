f3df1386c5ebda86a16296bfb2378d9afd57f6b2
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-11 15:08:58 -0400

    constructor injection

--- 05-Interfaces/src/com/fabrizio/Main.java
+++ 05-Interfaces/src/com/fabrizio/Main.java
@@ -3,6 +3,7 @@ package com.fabrizio;
 public class Main {
 
     public static void main(String[] args) {
-
+    var calculator = new TaxCalculator2018(100_000;
+    var report = new TaxReport(calculator);
     }
 }
