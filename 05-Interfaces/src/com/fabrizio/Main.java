627b9c634fedb3cac881cacb2750dcd3b971f230
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-12 17:43:23 -0400

    fields

--- 05-Interfaces/src/com/fabrizio/Main.java
+++ 05-Interfaces/src/com/fabrizio/Main.java
@@ -3,9 +3,10 @@ package com.fabrizio;
 public class Main {
 
     public static void main(String[] args) {
-   var calculator = new TaxCalculator2018(100_000);
-   var report = new TaxReport();
-   report.show(calculator);
-   report.show(new TaxCalculator2019());
+    }
+
+    public void doSomething(TaxCalculator calculator) {
+//        TaxCalculator.minimumTax
+
     }
 }
